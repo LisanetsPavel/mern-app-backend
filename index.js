daf2727d8572314a6b3e1ed0a6da92c1528460be
p.lysanets <p.lysanets@aimprosoft.com>
2023-03-29 10:09:40 +0300

    add log

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+console.log('process.env', process.env);
+
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch(( error ) => console.log(error.message))
